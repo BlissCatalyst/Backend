9a0d2e14588cf17ece85806af022a86781d805e2
Logan <46465575+BlissCatalyst@users.noreply.github.com>
2022-03-04 15:01:50 -0700

    Update middleware.js

--- api/middleware.js
+++ api/middleware.js
@@ -2,8 +2,19 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const whitelist = ["https://moodibeats.herokuapp.com/", "https://fantabulous-music-finder.herokuapp.com/"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 module.exports = server => {
-  server.use(cors());
+  server.use(cors(corsOptions));
   server.use(helmet());
   server.use(express.json());
 };
