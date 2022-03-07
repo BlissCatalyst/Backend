94d777ff70ff16a1aba2fb63bf7fdc855b5cbde7
Logan <46465575+BlissCatalyst@users.noreply.github.com>
2022-03-07 11:33:31 -0700

    Taking out whitelist sites
    CORS is supposed to allow all by default anyway.

--- api/middleware.js
+++ api/middleware.js
@@ -2,16 +2,16 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const whitelist = ["https://moodibeats.herokuapp.com/", "https://fantabulous-music-finder.herokuapp.com/"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = ["https://moodibeats.herokuapp.com/", "https://fantabulous-music-finder.herokuapp.com/"]
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 module.exports = server => {
   server.use(cors(corsOptions));
